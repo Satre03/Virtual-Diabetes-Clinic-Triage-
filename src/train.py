c9217071a1f90cc9ef268af612c1930ff2928869
Satre03 <154280522+Satre03@users.noreply.github.com>
2025-10-19 22:02:25 +0200

    Update train.py

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import datetime, timezone
 import json
 import joblib
+import numpy as np
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
